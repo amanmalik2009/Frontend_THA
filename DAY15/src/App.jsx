c0597fb2ada32bdee2345801d9654ae1b696eaef
AMAN MALIK <63892411+amanmalik2009@users.noreply.github.com>
2021-07-11 18:57:13 +0530

    Add files via upload

---
+++ DAY15/src/App.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      
+      <h1>whohoo this is my snowman card</h1>
+
+      <div className="cal-app-container">
+       <img src="https://images.unsplash.com/photo-1610898564097-e28bd69740a4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1234&q=80" alt="" />
+       
+      </div>
+    </div>
+  );
+}
